f8b9fdf47f99a7840d282df2d476aa6f7b1f48ae
Gitolite Okaski <me@gibzzz.tk>
2022-01-30 10:47:56 +0300

    Sentry DSN in .env

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import store from "./store";
 import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 
+const sentryDsn = import.meta.env.VITE_APP_SENTRY_DSN
+
 const app = createApp(App)
 
 Sentry.init({
     app,
-    dsn: "https://27098942e0124d8cafe2f06a11d051d0@o1115805.ingest.sentry.io/6148231",
+    dsn: sentryDsn,
     integrations: [
         new Integrations.BrowserTracing({
             routingInstrumentation: Sentry.vueRouterInstrumentation(router),
